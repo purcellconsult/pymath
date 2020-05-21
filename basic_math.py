4762299d9ca7078f2b73d5a2fd6176f94a9b2dec
purcellconsult <purcellconsult@gmail.com>
2020-05-21 14:25:05 -0700

    Add files via upload
    uplaoding pymath file.

---
+++ basic_math.py
@@ -0,0 +1,51 @@
+"""
+A module that emulates the functionality of:
+-- add
+-- subtract
+-- divide
+-- multiply
+"""
+
+
+def add(x, y):
+    """
+    This function takes in x and y
+    and then adds them.
+    :param x: Any number
+    :param y: Any number
+    :return: The sum of x and y
+    """
+    return x + y
+
+
+def subtract(x, y):
+    """
+    This function takes in x and y
+    and then subtracts them.
+    :param x: Any number
+    :param y: Any number
+    :return: The subtraction of x and y
+    """
+    return x - y
+
+
+def multiply(x, y):
+    """
+    This function takes in x and y
+    and then multiplies them.
+    :param x: Any number
+    :param y: Any number
+    :return: The multiplication of x and y
+    """
+    return x * y
+
+
+def divide(x, y):
+    """
+    This function takes in x and y
+    and then divides them.
+    :param x: Any number
+    :param y: Any number
+    :return: The division of x and y
+    """
+    return x / y
